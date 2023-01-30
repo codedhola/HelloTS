31fad75578726c17fa7fa95358a0723234fa2b85
codedhola <codedhola2000@gmail.com>
2023-01-30 11:54:18 +0100

    fix🔧: fix promise issues with try-catch

--- dist/controllers/artistController.js
+++ dist/controllers/artistController.js
@@ -17,57 +17,99 @@ const db_1 = require("./../config/db");
 const artistQuery_1 = __importDefault(require("./../database/queries/artistQuery"));
 // @get All artist => '/api/v1/artist'
 const getAllArtist = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const response = yield db_1.pool.query(artistQuery_1.default.getAllArtist);
-    res.status(200).json({
-        status: "Success",
-        results: response.rows.length,
-        data: {
-            data: response.rows
-        }
-    });
+    try {
+        const response = yield db_1.pool.query(artistQuery_1.default.getAllArtist);
+        return res.status(200).json({
+            status: "Success",
+            results: response.rows.length,
+            data: {
+                data: response.rows
+            }
+        });
+    }
+    catch (err) {
+        return res.status(500).json({
+            status: "Failed",
+            message: err
+        });
+    }
 });
 exports.getAllArtist = getAllArtist;
 // @get a single artist => '/api/v1/artist/;id'
 const getAnArtist = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const id = req.params.id;
-    const response = yield db_1.pool.query(artistQuery_1.default.getAnArtist, [id]);
-    res.status(200).json({
-        status: "Success",
-        data: {
-            data: response.rows[0]
-        }
-    });
+    try {
+        const response = yield db_1.pool.query(artistQuery_1.default.getAnArtist, [id]);
+        return res.status(200).json({
+            status: "Success",
+            data: {
+                data: response.rows[0]
+            }
+        });
+    }
+    catch (err) {
+        return res.status(500).json({
+            status: "Failed",
+            message: err
+        });
+    }
 });
 exports.getAnArtist = getAnArtist;
 // @create an artist  => '/api/v1/artist'
 const createArtist = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { name, dob, country } = req.body;
-    const response = yield db_1.pool.query(artistQuery_1.default.createArtist, [name, dob, country]);
-    res.status(200).json({
-        status: "Success",
-        data: {
-            data: response.rows[0]
-        }
-    });
+    try {
+        const response = yield db_1.pool.query(artistQuery_1.default.createArtist, [name, dob, country]);
+        return res.status(200).json({
+            status: "Success",
+            data: {
+                data: response.rows[0]
+            }
+        });
+    }
+    catch (err) {
+        return res.status(500).json({
+            status: "Failed",
+            message: err
+        });
+    }
 });
 exports.createArtist = createArtist;
 // @Edit an artist  => '/api/v1/artist/:id'
 const editArtist = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { name, dob, country } = req.body;
     const id = req.params.id;
-    const response = yield db_1.pool.query(artistQuery_1.default.editArtist, [name, dob, country, id]);
-    res.status(200).json({
-        status: "Success",
-        data: {
-            data: response.rows[0]
-        }
-    });
+    console.log(name);
+    try {
+        const response = yield db_1.pool.query(artistQuery_1.default.editArtist, [name, dob, country, id]);
+        console.log(response);
+        return res.status(200).json({
+            status: "Success",
+            data: {
+                data: response.rows[0]
+            }
+        });
+    }
+    catch (err) {
+        return res.status(500).json({
+            status: "Failed",
+            message: err
+        });
+    }
 });
 exports.editArtist = editArtist;
 // @Edit an artist  => '/api/v1/artist/:id'
 const deleteArtist = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const id = req.params.id;
-    const response = yield db_1.pool.query(artistQuery_1.default.deleteArtist, [id]);
-    res.status(204).json();
+    try {
+        const response = yield db_1.pool.query(artistQuery_1.default.deleteArtist, [id]);
+        return res.status(204).json();
+    }
+    catch (err) {
+        return res.status(500).json({
+            status: "Failed",
+            message: err
+        });
+    }
 });
 exports.deleteArtist = deleteArtist;
--- src/controllers/artistController.ts
+++ src/controllers/artistController.ts
@@ -4,62 +4,104 @@ import { pool } from './../config/db'
 import DB from './../database/queries/artistQuery'
 
 // @get All artist => '/api/v1/artist'
-export const getAllArtist = async (req: Request, res: Response) => {
-    const response: QueryResult = await pool.query(DB.getAllArtist)
-    
-    res.status(200).json({
-        status: "Success",
-        results: response.rows.length,
-        data: { 
-            data: response.rows
+export const getAllArtist = async (req: Request, res: Response): Promise<Response> => {
+    try{
+        const response: QueryResult = await pool.query(DB.getAllArtist)
+        
+        return res.status(200).json({
+            status: "Success",
+            results: response.rows.length,
+            data: { 
+                data: response.rows
+            }
+        })
+    }catch(err){
+        return res.status(500).json({
+            status: "Failed",
+            message: err
         }
-    })
+        )
+    }
 }
 
 // @get a single artist => '/api/v1/artist/;id'
-export const getAnArtist = async (req: Request, res: Response) => {
+export const getAnArtist = async (req: Request, res: Response): Promise<Response> => {
     const id: string = req.params.id
-    const response: QueryResult = await pool.query(DB.getAnArtist, [id])
-    
-    res.status(200).json({
-        status: "Success",
-        data: {
-            data: response.rows[0]
+    try{
+        const response: QueryResult = await pool.query(DB.getAnArtist, [id])
+        
+        return res.status(200).json({
+            status: "Success",
+            data: {
+                data: response.rows[0]
+            }
+        })
+    }catch(err){
+        return res.status(500).json({
+            status: "Failed",
+            message: err
         }
-    })
+        )
+    }
 }
 
 // @create an artist  => '/api/v1/artist'
-export const createArtist = async (req: Request, res: Response) => {
+export const createArtist = async (req: Request, res: Response): Promise<Response>  => {
     const { name, dob, country} = req.body
-    const response: QueryResult = await pool.query(DB.createArtist, [name, dob, country])
-    
-    res.status(200).json({
-        status: "Success",
-        data: {
-            data: response.rows[0]
+    try{
+        const response: QueryResult = await pool.query(DB.createArtist, [name, dob, country])
+        
+        return res.status(200).json({
+            status: "Success",
+            data: {
+                data: response.rows[0]
+            }
+        })
+    }catch(err){
+        return res.status(500).json({
+            status: "Failed",
+            message: err
         }
-    })
+        )
+    }
 }
 
 // @Edit an artist  => '/api/v1/artist/:id'
-export const editArtist = async (req: Request, res: Response) => {
+export const editArtist = async (req: Request, res: Response): Promise<Response> => {
     const { name, dob, country} = req.body
     const id: string = req.params.id
-    const response: QueryResult = await pool.query(DB.editArtist, [name, dob, country, id])
-    
-    res.status(200).json({
-        status: "Success",
-        data: {
-            data: response.rows[0]
+    console.log(name) 
+    try{
+        const response: QueryResult = await pool.query(DB.editArtist, [name, dob, country, id])
+        console.log(response)
+
+        return res.status(200).json({
+            status: "Success",
+            data: {
+                data: response.rows[0]
+            }
+        })
+    }catch(err){
+        return res.status(500).json({
+            status: "Failed",
+            message: err
         }
-    })
+        )
+    }
 }
 
 // @Edit an artist  => '/api/v1/artist/:id'
-export const deleteArtist = async (req: Request, res: Response) => {
+export const deleteArtist = async (req: Request, res: Response): Promise<Response> => {
     const id: string = req.params.id
-    const response: QueryResult = await pool.query(DB.deleteArtist, [id])
-    
-    res.status(204).json()
+    try{
+        const response: QueryResult = await pool.query(DB.deleteArtist, [id])
+        
+        return res.status(204).json()
+    }catch(err){
+        return res.status(500).json({
+            status: "Failed",
+            message: err
+        }
+        )
+    }
 }
